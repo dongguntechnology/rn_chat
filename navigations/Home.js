92d6f5984c8c12399f3311e39cd71c5713a7d4eb
dongguntechnology <shafeel2@naver.com>
2022-02-28 02:24:08 +0900

    탭 버튼과 헤더 타이틀

--- navigations/Home.js
+++ navigations/Home.js
@@ -3,16 +3,64 @@ import {ThemeContext} from 'styled-components/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {ChannelList, Profile} from '../screens';
 import {MaterialIcons} from '@expo/vector-icons';
-//import {getFocusedRouteNameFromRoute} from '@react-navigation/native';
+// 현재선택된 화면의 이름을 알려줌
+import {getFocusedRouteNameFromRoute} from '@react-navigation/native';
+
+const TabIcon = ({name, focused}) => {
+   const theme = useContext(ThemeContext);
+   return (
+      <MaterialIcons
+         name={name}
+         size={26}
+         color={focused ? theme.tabBtnActive : theme.tabBtnInactive}
+      />
+   );
+};
 
 const Tab = createBottomTabNavigator();
 
-const Home = ({}) => {
-   //navigation, route
+const Home = ({navigation, route}) => {
+   console.log(route);
+   useEffect(() => {
+      const screenName = getFocusedRouteNameFromRoute(route) || 'List';
+      navigation.setOptions({
+         headerTitle: screenName,
+         headerRight: () =>
+            screenName === 'List' && (
+               <MaterialIcons
+                  name="add"
+                  size={26}
+                  style={{margin: 10}}
+                  onPress={() => navigation.navigate('ChannelCreation')}
+               />
+            ),
+      });
+   });
+
    return (
       <Tab.Navigator>
-         <Tab.Screen name="목록" component={ChannelList} />
-         <Tab.Screen name="Profile" component={Profile} />
+         <Tab.Screen
+            name="목록"
+            component={ChannelList}
+            options={{
+               tabBarIcon: ({focused}) =>
+                  TabIcon({
+                     name: focused ? 'chat-bubble' : 'chat-bubble-outline',
+                     focused,
+                  }),
+            }}
+         />
+         <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+               tabBarIcon: ({focused}) =>
+                  TabIcon({
+                     name: focused ? 'person' : 'person-outline',
+                     focused,
+                  }),
+            }}
+         />
       </Tab.Navigator>
    );
 };
